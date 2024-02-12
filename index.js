891e48cee9dafc6d05e6a9a481922d4f5893a8af
JellyFished <gisele0kms@gmail.com>
2024-02-12 18:43:41 -0500

    DOM and victory

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+main.remove();
+
+const newHeader = document.querySelector("h1");
+
+newHeader.id = "victory";
+
+document.getElementById("victory").textContent = "Yes is the champion";
